0f220b320acf2b382877c0d399b0f24879af5a37
Fauzan Taslim <fauzantaslim123@gmail.com>
2026-03-24 21:34:51 +0700

    feat: change url for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://your-domain.com', // Update with actual domain
+      url: 'https://fauzantaslim.my.id',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
